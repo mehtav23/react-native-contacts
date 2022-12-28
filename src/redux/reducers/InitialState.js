f5a2607b849322663e12e8c421c31eee5966aaf6
Varun Mehta <varunmehta@Varuns-MacBook-Pro.local>
2022-12-28 14:25:36 +0530

    Pushing dummy data

---
+++ src/redux/reducers/InitialState.js
@@ -0,0 +1,74 @@
+export default {
+    contacts: [
+        {
+            id: '876af715-d8bc-4106-8a2e-291efc1c37e9',
+            firstName: 'Arnav',
+            lastName: 'Kumar',
+            primaryNumber: '+918080842761',
+            email: 'arnav123@gmail.com'
+        },
+        {
+            id: 'b563d04c-dec4-44ea-a1e7-c7e5c65d855f',
+            firstName: 'Deepesh',
+            lastName: 'Singh',
+            primaryNumber: '+918080842762',
+            email: 'singh123@gmail.com'
+        },
+        {
+            id: 'ac0d1bb2-adf2-418c-8cac-5c6ffb0e7541',
+            firstName: 'Esha',
+            lastName: 'Devi',
+            primaryNumber: '+918080842763',
+            email: 'esha1223@gmail.com'
+        },
+        {
+            id: '09720c12-bcc7-4ca4-b272-68810f637170',
+            firstName: 'Lovkesh',
+            lastName: 'Bajaj',
+            primaryNumber: '+918080842764',
+            email: 'bajajl5623@gmail.com'
+        },
+        {
+            id: 'dd4668c2-8d75-4a89-91d5-3e68a9dda8be',
+            firstName: 'Neha',
+            lastName: 'Mehta',
+            primaryNumber: '+918080842766',
+            email: 'mehtan123@gmail.com'
+        },
+        {
+            id: 'cf06054d-b869-42a2-9029-642682bb41b5',
+            firstName: 'OP',
+            lastName: 'Sharma',
+            primaryNumber: '+918080842767',
+            email: 'sharma23@gmail.com'
+        },
+        {
+            id: 'e092f118-f960-4137-ba4b-9c9f574bd7e3',
+            firstName: 'Tarun',
+            lastName: 'Mehta',
+            primaryNumber: '+918080842768',
+            email: 'mitre123@gmail.com'
+        },
+        {
+            id: 'da875ee0-0c3c-4c20-91d0-630ad39728bc',
+            firstName: 'Varun',
+            lastName: 'Mehta',
+            primaryNumber: '+918080842769',
+            email: 'meva12@gmail.com'
+        },
+        {
+            id: '068df8ed-801d-4673-8c26-3383e6653b0c',
+            firstName: 'Zeeshan',
+            lastName: 'Khan',
+            primaryNumber: '+918080842770',
+            email: 'zee123@gmail.com'
+        },
+        {
+            id: '9f459168-3ec3-4688-8951-0632076be726',
+            firstName: 'Zyed',
+            lastName: 'Khan',
+            primaryNumber: '+918080842771',
+            email: 'Khan123@gmail.com'
+        },
+    ]
+}
